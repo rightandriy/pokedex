6f715d0175b8f0c5acab7f751a675d87aaa83461
rightandriy <andriy.privalov@gmail.com>
2016-09-12 21:31:49 +0300

    Added Alomofire pod

--- pokedex/PokemonDetailedVC.swift
+++ pokedex/PokemonDetailedVC.swift
@@ -11,6 +11,29 @@ import UIKit
 class PokemonDetailedVC: UIViewController {
     
     @IBOutlet weak var nameLbl: UILabel!
+    
+    @IBOutlet weak var mainImage: UIImageView!
+    
+    @IBOutlet weak var descriptionLbl: UILabel!
+    
+    @IBOutlet weak var typeLbl: UILabel!
+    
+    @IBOutlet weak var defenseLbl: UILabel!
+    
+    @IBOutlet weak var heightLbl: UILabel!
+    
+    @IBOutlet weak var pokedexIdLbl: UILabel!
+    
+    @IBOutlet weak var weightLbl: UILabel!
+    
+    @IBOutlet weak var baseAttackLbl: UILabel!
+    
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    
+    @IBOutlet weak var evoLbl: UILabel!
+    
     var pokemon: Pokemon!
     
 
@@ -21,6 +44,9 @@ class PokemonDetailedVC: UIViewController {
     
     }
 
+    @IBAction func backBtnPressed(_ sender: AnyObject) {
+        dismiss(animated: true, completion: nil)
+    }
    
 
 }
